9ee8f7fcb9cda40ee020afb620581cfbe350a125
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-24 01:05:26 -0500

    Init

---
+++ src/test/java/com/tenstech/todo_core/TodoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenstech.todo_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
