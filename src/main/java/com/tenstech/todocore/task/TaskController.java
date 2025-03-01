a51a499d021666d6d2a8e9f3d7cda26feef56542
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-28 23:04:50 -0500

    Task Controller List

--- src/main/java/com/tenstech/todocore/task/TaskController.java
+++ src/main/java/com/tenstech/todocore/task/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -22,5 +24,11 @@ public class TaskController {
         return taskService.getById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        List<TaskDTO> taskDTOs = taskService.getAll();
+        return ResponseEntity.ok(taskDTOs);
     }
 }
