580df4e7161ec58f1ca70e420b8467fe487672d9
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-28 23:06:02 -0500

    Task Controller Create and Update

--- src/main/java/com/tenstech/todocore/task/TaskController.java
+++ src/main/java/com/tenstech/todocore/task/TaskController.java
@@ -1,13 +1,12 @@
 package com.tenstech.todocore.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tasks")
@@ -30,5 +29,24 @@ public class TaskController {
     public ResponseEntity<List<TaskDTO>> getAllTasks() {
         List<TaskDTO> taskDTOs = taskService.getAll();
         return ResponseEntity.ok(taskDTOs);
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+        Optional<TaskDTO> createdTaskDTO = taskService.createTask(taskDTO);
+        return createdTaskDTO.map(dto -> ResponseEntity.status(HttpStatus.CREATED).body(dto))
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+
+        if (!taskDTO.getId().equals(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Optional<TaskDTO> updatedTaskDTO = taskService.updateTask(taskDTO);
+        return updatedTaskDTO.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
