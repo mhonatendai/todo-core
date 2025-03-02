0fee4b5e233cd303427e7f078a2be1601f4e8148
Tendai E. Mhona <mhonatendai@gmail.com>
2025-03-02 02:10:01 -0500

    Repo

--- src/main/java/com/tenstech/todocore/task/TaskRepository.java
+++ src/main/java/com/tenstech/todocore/task/TaskRepository.java
@@ -1,7 +1,15 @@
 package com.tenstech.todocore.task;
 
+import com.tenstech.todocore.common.TaskStatus;
 import com.tenstech.todocore.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task,Long> {
+
+    List<Task> findAllByTaskStatus(TaskStatus taskStatus);
+
+    List<Task> findAllByDueDate(LocalDateTime dueDate);
 }
