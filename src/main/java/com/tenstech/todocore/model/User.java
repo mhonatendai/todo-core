c95cfea05efd0633de7a4b83493722efc56ede4f
Tendai E. Mhona <mhonatendai@gmail.com>
2025-03-03 18:11:23 -0500

    User init

---
+++ src/main/java/com/tenstech/todocore/model/User.java
@@ -0,0 +1,29 @@
+package com.tenstech.todocore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+public class User extends AuditedEntity{
+
+    @Column(name = "user_name", unique = true)
+    @NonNull
+    private String username;
+
+    @NonNull
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NonNull
+    @JsonIgnore
+    private String password;
+
+}
